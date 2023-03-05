5dc02bcec087f84d1133f4722b2e0c0ca591f75a
John Parkhurst <jparkhurst120@gmail.com>
2023-03-05 00:28:22 -0500

    Updates

---
+++ precache-manifest.8e72006d475bc39368143876ac2e4f7b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a2f84d9aa2ec86029796",
+    "url": "/Wow-Ranking/static/css/main.70b2e842.chunk.css"
+  },
+  {
+    "revision": "a2f84d9aa2ec86029796",
+    "url": "/Wow-Ranking/static/js/main.a2f84d9a.chunk.js"
+  },
+  {
+    "revision": "57fb75a2e8592a1edd30",
+    "url": "/Wow-Ranking/static/js/1.57fb75a2.chunk.js"
+  },
+  {
+    "revision": "4b48df7e80a592757cb6",
+    "url": "/Wow-Ranking/static/js/2.4b48df7e.chunk.js"
+  },
+  {
+    "revision": "c362315de962a7eb1940",
+    "url": "/Wow-Ranking/static/js/runtime~main.c362315d.js"
+  },
+  {
+    "revision": "b261e9046cdaf0cfcf6adde4476fc1e8",
+    "url": "/Wow-Ranking/index.html"
+  }
+];
